743ce5a9eabcd1eb7898ff05cbeb0ac93e162a5f
Demetry Pascal <qtckpuhdsa@gmail.com>
2022-04-13 00:42:00 +0300

    v6.7.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geneticalgorithm2", 
-    version="6.7.2",
+    version="6.7.3",
     author="Demetry Pascal",
     author_email="qtckpuhdsa@gmail.com",
     maintainer='Demetry Pascal',
